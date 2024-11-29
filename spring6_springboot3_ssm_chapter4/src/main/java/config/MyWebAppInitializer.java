de19b56729a02b860ab0a5846df51c1b71733f83
cp <2856838704@qq.com>
2024-11-29 22:49:19 +0800

    更新结构

--- spring6_springboot3_ssm_chapter4/src/main/java/config/MyWebAppInitializer.java
+++ spring6_springboot3_ssm_chapter4/src/main/java/config/MyWebAppInitializer.java
@@ -1,4 +1,4 @@
-package com.chanpller.spring6_springboot3_ssm.chapter4.config;
+package config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
--- spring6_springboot3_ssm_chapter4/src/main/java/controller/HelloController.java
+++ spring6_springboot3_ssm_chapter4/src/main/java/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.chanpller.spring6_springboot3_ssm.chapter4.controller;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
